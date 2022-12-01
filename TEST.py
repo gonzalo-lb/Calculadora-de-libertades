f7159205ff4ca402873d36782e43aca5184c7384
gonzalo-lb <gonzalo.lb@hotmail.com>
2022-12-01 08:27:05 -0600

    Fecha Input
    Lo que hice ahora:
     - Estoy reformando la función de input de fecha, para que acepte formato de pocos dígitos.
    
    Con qué seguir:
     - Unidades fijas
     - Interfaz para el usuario
     - Lo del cómputo por LC revocada
     - Agregar en el __init__ de Computo() un chequeo para que se fije qué tipo de cálculo va a hacer (pena temporal, ejecución condicional, etc) y luego revise que se hayan ingresado los parámetros concreto de esa situación
     - Terminar vencimiento y caducidad para el resto de las penas.
     - Reformar los cálculos de vencimiento y otros para la reclusión por tiempo indeterminado
     - Implementar cálculos para casos de reclusión por tiempo indeterminado
     - Integrar las consultas a la devolución que se hace con el cómputo
     - EN ST y LC, agregar un else para los casos de que no sean ninguno de los regímenes (no debería suceder pero...)
     - Cuando indique los regímenes de detención, corregir las nomenclaturas para que indique la ley base y la modificación
    
    Funcionalidad a implementar:
     - Nuevo cómputo para libertad condicional revocada (no se contabiliza el tiempo que se estuvo en libertad)
     - Alertas por reincidencia, delitos excluidos del régimen de libertades (por ley 27.375), libertades revocadas, etc. con indicación de la normativa específica.
     - Requisito temporal extra para la ST, de permanencia en periodo de prueba (art. 17, ley 24.660)
     - Cálculos para la reclusión por tiempo indeterminado (art. 52 CP)
     - Régimen de salida anticipada para los casos del 56 bis (1 año antes)
     - Monto de las Unidades Fijas

--- TEST.py
+++ TEST.py
@@ -1,1 +1,65 @@
-a = input()
+import datetime
+
+def GetConsoleInput_Fecha():    
+    user_input = input('Fecha: ')
+    if len(user_input) < 6 or len(user_input) > 10:
+        print('ERROR: No se ingresó una fecha válida')
+        return
+
+    cursor = 0
+    number_start = 0
+    number_end = 0    
+    fecha = []
+
+    for x in range(2):
+        for y in range(3):
+            if user_input[cursor] == "/":
+                number_end = cursor - 1
+                break
+            else:
+                cursor += 1
+        if number_start == number_end:
+            fecha.append(int(user_input[number_start]))
+        else:
+            fecha.append(int(user_input[number_start:number_end+1]))
+        
+        cursor += 1
+        number_start = cursor
+    
+    # Para el año, busca desde el fondo
+    number_end = cursor = len(user_input) - 1
+    for y in range(5):
+        if user_input[cursor] == "/":
+            number_start = cursor + 1
+            break
+        else:
+            cursor -= 1
+    
+    # Revisa que los años tengan 2 dígitos o 4
+    year_digits = len(user_input[number_start:number_end+1])
+    if year_digits != 2 and year_digits != 4:
+        print('ERROR: No se ingresó una fecha válida')
+        return
+    
+    year = int(user_input[number_start:number_end+1])
+
+    # Si el año ingresado es de dos dígitos, lo pasa a formato de 4 dígitos
+    if year_digits == 2:
+        if year <= 60:
+            year += 2000
+        else:
+            year += 1900
+
+    fecha.append(year)
+
+    print(fecha)
+
+
+GetConsoleInput_Fecha()
+a = '1/1/22'
+b = '01/01/2022'
+
+print(len(a))
+print(len(b))
+
+    
